f169a627b21328316d2aae5e4bc97150cc4bf6d1
Alienxbe <martin.ykman@gmail.com>
2022-08-10 07:06:48 +0200

    Fixed function

--- srcs/window/ft_put_image_to_window.c
+++ srcs/window/ft_put_image_to_window.c
@@ -6,7 +6,7 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 06:24:07 by mykman            #+#    #+#             */
-/*   Updated: 2022/08/10 07:05:31 by mykman           ###   ########.fr       */
+/*   Updated: 2022/08/10 07:06:37 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_bool	ft_put_image_to_window(void *mlx_ptr, t_window win, t_img img,
 	if (!issmaller_point(pos, win.size)
 		|| !issmaller_point(add_point(pos, img.size),
 			add_point(win.size, (t_point){1, 1}))
-		|| issmaller_point(pos, (t_point){0, 0}))
+		|| !isbigger_point(pos, (t_point){-1, -1}))
 		return (false);
 	mlx_put_image_to_window(mlx_ptr, win.win_ptr, img.img, pos.x, pos.y);
 	return (true);
