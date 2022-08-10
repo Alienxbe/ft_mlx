affa2b118ae3e0fd5da51b091390272ab9ea5899
Alienxbe <martin.ykman@gmail.com>
2022-08-10 07:14:53 +0200

    Protecting ft_new_window

--- srcs/window/ft_new_window.c
+++ srcs/window/ft_new_window.c
@@ -6,7 +6,7 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 06:14:52 by mykman            #+#    #+#             */
-/*   Updated: 2022/08/10 07:02:59 by mykman           ###   ########.fr       */
+/*   Updated: 2022/08/10 07:14:32 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ t_window	ft_new_window(void *mlx_ptr, t_point size, char *name, int (*f)())
 		mlx_destroy_window(mlx_ptr, win.win_ptr);
 		return ((t_window){0});
 	}
-	mlx_hook(win.win_ptr, 17, 0, f, NULL);
+	if (f)
+		mlx_hook(win.win_ptr, 17, 0, f, NULL);
 	win.size = size;
 	win.name = name;
 	return (win);
