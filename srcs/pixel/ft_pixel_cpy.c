51797419431d4c55e8b5baf03d20774935b262c5
Alienxbe <martin.ykman@gmail.com>
2022-08-10 05:22:53 +0200

    Repairing function: - Forgot parentheses

--- srcs/pixel/ft_pixel_cpy.c
+++ srcs/pixel/ft_pixel_cpy.c
@@ -6,7 +6,7 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 22:51:54 by mykman            #+#    #+#             */
-/*   Updated: 2022/08/10 05:19:31 by mykman           ###   ########.fr       */
+/*   Updated: 2022/08/10 05:22:28 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_bool	ft_pixel_cpy(t_img src, t_point p_src, t_img dst, t_point p_dst)
 	if (!issmaller_point(p_src, src.size) || !issmaller_point(p_dst, dst.size))
 		return (false);
 	pixel_color = ft_pixel_get_color(src, p_src);
-	if (pixel_color & 0xFF000000 == 0xFF000000)
+	if ((pixel_color & 0xFF000000) == 0xFF000000)
 		return (true);
 	return (ft_pixel_put(dst, p_dst, pixel_color));
 }
